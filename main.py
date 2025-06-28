afe5bfd9ffc21dacbbbe3e5cd7cb4a6cea1a0524
MarquezziniBL <145385548+MarquezziniBL@users.noreply.github.com>
2025-06-28 18:13:08 -0300

    Alteração - 1.3.10
    Implementado:
    
    - Comparativo de consumo com mês anterior;
    
    Melhorado:
    
    - função de consumo para aceitar o comparativo;
    
    - estrutura de dados da função analise_gemma;
    
    Implementar:
    
    - Continuar  colocando informações no checkbox de análise para comparação de dados entre o mês atual e dados anteriores tipo meses, semanas e etc.
    
    - criar gráfico de consumo mensal da dependência.
    
    Melhorar:
    
    - Nomes dos widgets tipo figuras e containers.

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from datetime import date
 genai.configure(api_key="")
 modelo_ai = genai.GenerativeModel('gemini-1.5-flash')
 
-versao = " Versão: 1.3.9"
+versao = " Versão: 1.3.10"
 
 lista_anos = [2024,2025]
 
@@ -27,8 +27,7 @@ class Dashboard():
             l_dias_semana = []
             try:
                 for i in dicicionario:
-                    data_editar = str(i).replace(" 00:00:00","")
-                            
+                    data_editar = str(i).replace(" 00:00:00","")     
                     l_data= data_editar.split(sep ="-",maxsplit=3)
                     ano,mes,dia = l_data[0],l_data[len(l_data)//2],l_data[-1]
                     data = date(int(ano),int(mes),int(dia))
@@ -38,26 +37,20 @@ class Dashboard():
             except ValueError:
                 pass
             return l_dias_semana
-    def analise_gemma (self):
-        try:
-            total = (f"{float(sum(self.l_soma)):0.2f}").replace(".",",")
-            hp = (f"{float(sum(self.l_hp)):0.2f}").replace(".",",")
-            hfp = (f"{float(sum(self.l_hfp)):0.2f}").replace(".",",")
-            media_mensal = (f"{float(sum((self.l_soma))/len(self.l_soma)):0.2f}").replace(".",",")
-            
-            if st.session_state["check"]:
-                st.session_state["test"] = True
-                self.container4.write(f"Análise de consumo da(o):  {self.selecao_opcoes_dependencia}")
-                self.container4.write(f"Total: {total} KWh -  dividido em HP = {hp} e HFP = {hfp}")
-                self.container4.write(f"Média mensal: {media_mensal} KWh")
-                self.container4.write("Em implementação")
-            else:
-                st.session_state["test"] = False
-                self.container4.write()
-        except ZeroDivisionError:
-                pass
-    def consumo(self,coluna):
+        
+    def consumo(self,situacao,coluna):
         df = pd.DataFrame(self.planilha_medicao, columns=[coluna+"-HFP", coluna+"-HP"])
+        if situacao == "analise":
+            mes_posicao = lista_meses.index(self.selecao_opcoes_mes)
+            self.planilha_medicao_analise = pd.read_excel(f"medicao_energia_{self.selecao_opcoes_ano}.xlsx", sheet_name=lista_meses[mes_posicao-1])
+            try:    
+                if self.selecao_opcoes_mes == "JANEIRO":
+                    self.planilha_medicao_analise = pd.read_excel(f"medicao_energia_{self.selecao_opcoes_ano-1}.xlsx", sheet_name=lista_meses[mes_posicao-1])
+            except FileNotFoundError:
+                self.container5.write(f"Não encontrado arquivo para o ano de {self.selecao_opcoes_ano-1}")
+                st.session_state["test"] = False
+            df = pd.DataFrame(self.planilha_medicao_analise, columns=[coluna+"-HFP", coluna+"-HP"])
+        
         self.lista_hfp = []
         self.lista_hp = []
         self.lista_soma = []
@@ -70,9 +63,49 @@ class Dashboard():
             self.lista_hfp.append(float(f"{i:.0f}"))
             self.lista_hp.append(float(f"{j:.0f}"))
             self.lista_soma.append(float(f"{i:.0f}")+float(f"{j:.0f}"))
-        
 
         return list(filter(lambda x: not np.isnan(x) and x > 0, self.lista_hfp)), list(filter(lambda x: not np.isnan(x) and x > 0, self.lista_hp)), list(filter(lambda x: not np.isnan(x) and x > 0, self.lista_soma))   
+    def analise_gemma (self):
+        l_hfp_mes_ant,l_hp_mes_ant,l_soma_mes_ant = self.consumo("analise",self.selecao_opcoes_dependencia)
+        try:
+            total_mes_atual = float(sum(self.l_soma))
+            total_mes_ant = float(sum(l_soma_mes_ant))
+            variacao = 0
+            if total_mes_ant == 0:
+                variacao = 0
+            else:
+                variacao = ((total_mes_atual/total_mes_ant)-1)*100
+            
+            
+            hp_mes_atual = (f"{float(sum(self.l_hp)):.0f}").replace(".",",")
+            hfp_mes_atual = (f"{float(sum(self.l_hfp)):.0f}").replace(".",",")
+            media_mensal_mes_atual = (f"{float(sum((self.l_soma))/len(self.l_soma)):.0f}").replace(".",",")
+            valor_max_total_mes_atual = (f"{max(self.l_soma):.0f}").replace(".",",")
+            valor_min_total_mes_atual = (f"{min(self.l_soma):.0f}").replace(".",",")
+            
+            tma = (f"{(total_mes_atual):.0f}").replace(".",",")
+            tmant = (f"{(total_mes_ant):.0f}").replace(".",",")
+            vari = (f"{(variacao):.0f}").replace(".",",")
+            if st.session_state["check"]:
+                st.session_state["test"] = True
+                l_analises = [
+                    f"Análise de consumo da(o) {self.selecao_opcoes_dependencia}",
+                    "<div style='text-align: center'> Mês Atual </div>"," ",
+                    f"Total:  {tma} KWh -  dividido em HP = {hp_mes_atual} e HFP = {hfp_mes_atual}",
+                    f"Maior Consumo:  {valor_max_total_mes_atual} KWh e Menor Consumo: {valor_min_total_mes_atual} KWh",
+                    f"Média Mensal:  {media_mensal_mes_atual} KWh",
+                    "<div style='text-align: center'> Comparativos </div>"," ",
+                    f"Total:",
+                    f"  1) Mês atual =  {tma} KWh -  Mês anterior = {tmant}, Variação de {vari}%"
+                    ]
+                
+                for i in l_analises:
+                    self.container5.markdown(i,unsafe_allow_html=True)
+            else:
+                st.session_state["test"] = False
+                self.container5.write()
+        except ZeroDivisionError:
+                self.container
     def info_centralizada(self):
         df = pd.DataFrame(self.planilha_medicao)
         lista_somas_consumo = []
@@ -158,13 +191,10 @@ class Dashboard():
             
         self.container2 = st.container() 
         with self.container2:
-            self.l_hfp,self.l_hp,self.l_soma = self.consumo(self.selecao_opcoes_dependencia)
+            self.l_hfp,self.l_hp,self.l_soma = self.consumo("normal",self.selecao_opcoes_dependencia)
             col12,col13 = st.columns([6,1],gap="small", vertical_alignment="center" )
             
             with col12:
-                
-                
-                
                 barra1 = go.Bar(x=lista_x, y=self.l_hfp, name="HFP", marker_color = "#00a2ff")
                 barra2 = go.Bar(x=lista_x, y=self.l_hp, name = "HP", marker_color = "#ff6600")
                 linha_soma = go.Scatter(x=lista_x, y=self.l_soma, name="HFP + HP" ,mode="markers+lines", 
@@ -178,13 +208,7 @@ class Dashboard():
                             hovermode = "x",
                             )
                 st.plotly_chart(self.fig2)
-                
-                if "test" not in st.session_state:
-                    st.session_state["test"] = False
-                self.analise_cons_ind = st.checkbox("Análise", value=st.session_state["test"],
-                    key="check", on_change= self.analise_gemma)
-                
-                
+
             with col13:
                 pizza1 = go.Pie(labels=["",""],values=[sum(self.l_hfp), sum(self.l_hp)], showlegend=False,
                             title="Percentual HFP x HP")
@@ -193,8 +217,16 @@ class Dashboard():
                     line = dict(color = "#13293D", width=2)), textfont_size = 16)
                 
                 st.plotly_chart(fig_pizza1)
-            self.container4 = st.container() 
+            
+            self.container4 = st.container()
             with self.container4:
+                if "test" not in st.session_state:
+                    st.session_state["test"] = False
+                self.analise_cons_ind = st.checkbox("Análise", value=st.session_state["test"],
+                    key="check", on_change= self.analise_gemma)
+            self.container5 = st.container() 
+            
+            with self.container5:
                 st.write()
 if __name__=="__main__":
     Dashboard()
