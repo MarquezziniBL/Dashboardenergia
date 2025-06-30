1e70ef3dc37248644b200b33f1f76f77ba4f4a27
MarquezziniBL <145385548+MarquezziniBL@users.noreply.github.com>
2025-06-30 14:49:47 -0300

    Alteração - 1.3.11
    Implementado:
    
    - Modificação de layout com CSS
    
    Melhorado:
    
    - Correção de erro protbuffer;
    
    Implementar:
    
    - Continuar colocando informações no checkbox de análise para comparação de dados entre o mês atual e dados anteriores tipo meses, semanas e etc.
    
    - criar gráfico de consumo mensal da dependência.
    
    Melhorar:
    
    - Nomes dos widgets tipo figuras e containers.
    
    - Layout widgets com CSS

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import plotly.io as pio
 import PIL
 import google.generativeai as genai
 from datetime import date
+import pathlib
 
 
 genai.configure(api_key="")
@@ -23,6 +24,9 @@ dict_dias_semana = {"0":"Seg","1":"Ter","2":"Qua","3":"Qui","4":"Sex","5":"Sáb"
 colunas = ["Data","SEDE","HTS", "HTO", "ALQQ", "USINA/CICRIN", "FADOR", "CIAFV 01", "CIAFV 02", "CIAFV 03"]
 
 class Dashboard():
+    def local_css(self,filename):
+        with open(filename) as fn:
+            st.markdown(f"<style>{fn.read()}</style>", unsafe_allow_html=True)
     def dias_semana (self,dicicionario):
             l_dias_semana = []
             try:
@@ -36,8 +40,7 @@ class Dashboard():
                     l_dias_semana.append( edit + "," + dia)
             except ValueError:
                 pass
-            return l_dias_semana
-        
+            return l_dias_semana 
     def consumo(self,situacao,coluna):
         df = pd.DataFrame(self.planilha_medicao, columns=[coluna+"-HFP", coluna+"-HP"])
         if situacao == "analise":
@@ -47,8 +50,8 @@ class Dashboard():
                 if self.selecao_opcoes_mes == "JANEIRO":
                     self.planilha_medicao_analise = pd.read_excel(f"medicao_energia_{self.selecao_opcoes_ano-1}.xlsx", sheet_name=lista_meses[mes_posicao-1])
             except FileNotFoundError:
-                self.container5.write(f"Não encontrado arquivo para o ano de {self.selecao_opcoes_ano-1}")
-                st.session_state["test"] = False
+                self.planilha_medicao_analise = self.planilha_medicao
+                self.container5.error(f"Não existe arquivo para {self.selecao_opcoes_ano-1}", width=500)
             df = pd.DataFrame(self.planilha_medicao_analise, columns=[coluna+"-HFP", coluna+"-HP"])
         
         self.lista_hfp = []
@@ -66,6 +69,7 @@ class Dashboard():
 
         return list(filter(lambda x: not np.isnan(x) and x > 0, self.lista_hfp)), list(filter(lambda x: not np.isnan(x) and x > 0, self.lista_hp)), list(filter(lambda x: not np.isnan(x) and x > 0, self.lista_soma))   
     def analise_gemma (self):
+        
         l_hfp_mes_ant,l_hp_mes_ant,l_soma_mes_ant = self.consumo("analise",self.selecao_opcoes_dependencia)
         try:
             total_mes_atual = float(sum(self.l_soma))
@@ -75,7 +79,6 @@ class Dashboard():
                 variacao = 0
             else:
                 variacao = ((total_mes_atual/total_mes_ant)-1)*100
-            
             
             hp_mes_atual = (f"{float(sum(self.l_hp)):.0f}").replace(".",",")
             hfp_mes_atual = (f"{float(sum(self.l_hfp)):.0f}").replace(".",",")
@@ -89,23 +92,24 @@ class Dashboard():
             if st.session_state["check"]:
                 st.session_state["test"] = True
                 l_analises = [
-                    f"Análise de consumo da(o) {self.selecao_opcoes_dependencia}",
-                    "<div style='text-align: center'> Mês Atual </div>"," ",
-                    f"Total:  {tma} KWh -  dividido em HP = {hp_mes_atual} e HFP = {hfp_mes_atual}",
-                    f"Maior Consumo:  {valor_max_total_mes_atual} KWh e Menor Consumo: {valor_min_total_mes_atual} KWh",
-                    f"Média Mensal:  {media_mensal_mes_atual} KWh",
-                    "<div style='text-align: center'> Comparativos </div>"," ",
-                    f"Total:",
+                    f" Análise de consumo da(o) {self.selecao_opcoes_dependencia}",
+                    " <div style='text-align: center'> Mês Atual </div>"," ",
+                    f" Total:  {tma} KWh -  dividido em HP = {hp_mes_atual} e HFP = {hfp_mes_atual}",
+                    f" Maior Consumo:  {valor_max_total_mes_atual} KWh e Menor Consumo: {valor_min_total_mes_atual} KWh",
+                    f" Média Mensal:  {media_mensal_mes_atual} KWh",
+                    " <div style='text-align: center'> Comparativos </div>"," ",
+                    f" Total:",
                     f"  1) Mês atual =  {tma} KWh -  Mês anterior = {tmant}, Variação de {vari}%"
                     ]
                 
                 for i in l_analises:
                     self.container5.markdown(i,unsafe_allow_html=True)
+                    
             else:
                 st.session_state["test"] = False
                 self.container5.write()
         except ZeroDivisionError:
-                self.container
+                self.container5.error(f"Dados para o mês zerados", width=500)
     def info_centralizada(self):
         df = pd.DataFrame(self.planilha_medicao)
         lista_somas_consumo = []
@@ -144,7 +148,7 @@ class Dashboard():
             with col5:
                 st.write(versao)
         
-        self.container = st.container(border=True)
+        self.container = st.container(border=True, key="container")
         with self.container:
             col7, col8= st.columns([1,3],gap="small", vertical_alignment="center" )
             with col7:
@@ -171,8 +175,7 @@ class Dashboard():
                 except Exception:
                     st.error(f"Provavelmente a planilha do mês de {self.selecao_opcoes_mes} está vazia", width=500)
 
-        self.container1 = st.container()
-        
+        self.container1 = st.container(key="container_um")
         with self.container1:
             fig = go.Figure()
             df = pd.DataFrame(self.planilha_medicao)
@@ -181,19 +184,19 @@ class Dashboard():
             for i in colunas:
                 fig.add_trace(go.Scatter(x=lista_x,y=df[i], name=i, mode="markers+lines"))
             fig.update_layout(
-                        title=f"Gráfico de medições",
+                        title={"text":"Gráfico de medições","x":0.5,"y":0.9,"xanchor" : "center", "yanchor":"top"},
                         yaxis_title="Medição",
                         xaxis_title="Data",
                         xaxis = dict(title="Data",range = [0,31]),
-                        hovermode = "x",
+                        hovermode = "x"
                     )
             st.plotly_chart(fig)
             
-        self.container2 = st.container() 
+        self.container2 = st.container(key="container_dois") 
         with self.container2:
             self.l_hfp,self.l_hp,self.l_soma = self.consumo("normal",self.selecao_opcoes_dependencia)
-            col12,col13 = st.columns([6,1],gap="small", vertical_alignment="center" )
             
+            col12,col13 = st.columns([6,1],gap="small", vertical_alignment="center" )
             with col12:
                 barra1 = go.Bar(x=lista_x, y=self.l_hfp, name="HFP", marker_color = "#00a2ff")
                 barra2 = go.Bar(x=lista_x, y=self.l_hp, name = "HP", marker_color = "#ff6600")
@@ -201,10 +204,12 @@ class Dashboard():
                     marker=dict(size=6, symbol="circle"), line=dict(width=3,color="red"))
                 self.fig2 = go.Figure(data=[barra1,barra2,linha_soma])
                 self.fig2.update_layout(
-                            title=f"Gráfico de Consumo Individualizado: {self.selecao_opcoes_dependencia}",
+                            title={"text":f"Gráfico de Consumo Individualizado: {self.selecao_opcoes_dependencia}",
+                                "x":0.5,"y":0.9,"xanchor" : "center", "yanchor":"top"},
                             yaxis = dict(title="Consumo", tickformat='.0f'),
                             xaxis = dict(title="Data"),
                             plot_bgcolor = "#b6d5ee",
+                            paper_bgcolor = "#006494",
                             hovermode = "x",
                             )
                 st.plotly_chart(self.fig2)
@@ -214,19 +219,22 @@ class Dashboard():
                             title="Percentual HFP x HP")
                 fig_pizza1 = go.Figure(data=[pizza1])
                 fig_pizza1.update_traces(marker=dict(colors=["#00a2ff","#ff6600"],
-                    line = dict(color = "#13293D", width=2)), textfont_size = 16)
-                
+                    line = dict(color = "#FFFFFF", width=1)), textfont_size = 12)
+                fig_pizza1.update_layout(
+                    paper_bgcolor = "#006494",
+                )
                 st.plotly_chart(fig_pizza1)
             
-            self.container4 = st.container()
+            self.container4 = st.container(key="container_quatro")
             with self.container4:
                 if "test" not in st.session_state:
                     st.session_state["test"] = False
                 self.analise_cons_ind = st.checkbox("Análise", value=st.session_state["test"],
                     key="check", on_change= self.analise_gemma)
-            self.container5 = st.container() 
             
+            self.container5 = st.container(key="container_cinco") 
             with self.container5:
                 st.write()
+        self.local_css(pathlib.Path("assets//style.css"))
 if __name__=="__main__":
     Dashboard()
